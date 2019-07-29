373cc8e5c3dee2873c4ff9318f23de2cb22710c6
Nina Golodets <ninagolodets@Ninas-MacBook-Pro.local>
2019-07-29 14:21:54 -0700

    added shelter locations and info file

---
+++ src/LOCATIONS.js
@@ -0,0 +1,86 @@
+const LOCATIONS = {
+  locations: [
+    {
+      "name": "University District Youth Center",
+      "adress": "4516 15th Ave NE, Seattle, WA 98105",
+      "zipcode": 98105,
+      "phone": "(206) 526-2992",
+      "website": "https://youthcare.org/blog/youthcare-welcomes-university-district-youth-center/",
+      "hours": {
+        "monday": "7:00am to 2:00pm",
+        "tuesday": "7:00am to 2:00pm",
+        "wednesday": "7:00am to 2:00pm",
+        "thursday": "7:00am to 2:00pm",
+        "friday": "7:00am to 2:00pm",
+        "saturday": "7:00am to 2:00pm",
+        "sunday": "7:00am to 2:00pm"
+      }
+    },
+    {
+      "name": "Orion Center",
+      "adress": "1828 Yale Ave, Seattle, WA 98101",
+      "zipcode": 98101,
+      "phone": "(206) 622-5555",
+      "website": "https://youthcare.org/homeless-youth-services/engagement-services/youthcares-orion-center/",
+      "hours": {
+        "monday": "12:00pm to 3:00pm",
+        "tuesday": "12:00pm to 3:00pm",
+        "wednesday": "12:00pm to 3:00pm",
+        "thursday": "Closed",
+        "friday": "12:00pm to 3:00pm",
+        "saturday": "Closed",
+        "sunday": "Closed"
+      }
+    },
+    {
+      "name": "Drop In and Young Adult Emergency Shelter",
+      "adress": "2709 3rd Avenue Seattle, WA 98121",
+      "zipcode": 98121,
+      "phone": "(206) 374-0866",
+      "website": "https://nhmin.org/",
+      "hours": {
+        "monday": "7:00pm to 9:00pm",
+        "tuesday": "7:00pm to 9:00pm",
+        "wednesday": "7:00pm to 9:00pm",
+        "thursday": "7:00pm to 9:00pm",
+        "friday": "Closed",
+        "saturday": "Closed",
+        "sunday": "7:00pm to 9:00pm"
+      }
+    },
+    {
+      "name": "Homeless Youth for Peace and Empowerment (HYPE)",
+      "adress": "1609 19th Ave, Seattle, WA 98122",
+      "zipcode": 98122,
+      "phone": "(206) 726-8500",
+      "website": "http://www.psks.org/",
+      "hours": {
+        "monday": "5:30am - 9:30pm",
+        "tuesday": "5:30am - 9:30pm",
+        "wednesday": "5:30am - 9:30pm",
+        "thursday": "5:30am - 9:30pm",
+        "friday": "5:30am - 9:30pm (Closed 11am - 2pm)",
+        "saturday": "5:30am - 9:30pm",
+        "sunday": "5:30am - 9:30pm"
+      }
+    },
+    {
+      "name": "Jackson Street",
+      "adress": "1253 S Jackson St, Seattle, WA 98144",
+      "zipcode": 98144,
+      "phone": "206-694-4500",
+      "website": "https://youthcare.org/homeless-youth-services/engagement-services/jackson-street/",
+      "hours": {
+        "monday": "1:00pm - 6:30pm",
+        "tuesday": "1:00pm - 6:30pm",
+        "wednesday": "1:00pm - 6:30pm",
+        "thursday": "1:00pm - 6:30pm",
+        "friday": "1:00pm - 6:30pm",
+        "saturday": "1:00pm - 6:30pm",
+        "sunday": "1:00pm - 6:30pm"
+      }
+    }
+  ]
+}
+
+export default LOCATIONS;
